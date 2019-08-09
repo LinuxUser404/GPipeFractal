3564fb37b790841e8061daaeba8fb1f220ccd5ac
LinuxUser404 <nikolay.pershyn@gmail.com>
2019-08-09 15:52:59 +0300

    moving towards cabal v2

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleContexts #-}
+--{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE BangPatterns #-}
+--{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE PackageImports #-}
 --{-# LANGUAGE TemplateHaskell #-}
 
-module Main where
+
+module Main where 
 
 import GHC.Conc (numCapabilities) -- check number of threads available
 
@@ -14,11 +15,12 @@ import Prelude hiding ((<*))
 import Graphics.GPipe   
 import qualified "GPipe-GLFW" Graphics.GPipe.Context.GLFW as GLFW  
 import Control.Monad (unless)  
+import Data.Tuple (swap)
 
 main = do
   putStrLn $ "Number of threads: " ++ show numCapabilities
   runContextT GLFW.defaultHandleConfig $ do
-    win <- newWindow (WindowFormatColorDepth RGB8 Depth16) (GLFW.defaultWindowConfig "OpenGL Graphics")
+    win <- newWindow (WindowFormatColorDepth RGB8 Depth16) (GLFW.WindowConfig 500 500 "GPipe Fractal" Nothing [] Nothing)
     vertexBuffer :: Buffer os (B4 Float, B3 Float) <- newBuffer 4  
     writeBuffer vertexBuffer 0 [ (V4 (-1) (-1) 0 1, V3 1 0 0)  
                                , (V4   1  (-1) 0 1, V3 0 1 0)  
@@ -28,7 +30,7 @@ main = do
                         
     shader <- compileShader $ do  
       primitiveStream <- toPrimitiveStream id
-      fragmentStream <- rasterize (const (Front, ViewPort (V2 0 0) (V2 1920 1080), DepthRange 0 1)) primitiveStream
+      fragmentStream <- rasterize (const (Front, ViewPort (V2 0 0) (V2 500 500), DepthRange 0 1)) primitiveStream
       let fragmentStream2 = myFragmentShader fragmentStream
       drawWindowColor (const (win, ContextColorOption NoBlending (V3 True True True))) fragmentStream2
       
@@ -47,27 +49,27 @@ mainLoop vertexBuffer shader win = do
     mainLoop vertexBuffer shader win
 
 
-
+myFragmentShader :: FragmentStream (V3 (S F Float)) -> FragmentStream (V3 (S F Float))
 myFragmentShader = withRasterizedInfo singleFragmentTransformer
 
 singleFragmentTransformer :: V3 (S F Float) -> RasterizedInfo -> V3 (S F Float)
-singleFragmentTransformer _ (RasterizedInfo (V4 x y _ _) _ _) = colorFunc $ Complex (x / 2000.0 - 2.0) (y / 1000.0 - 0.5)
+singleFragmentTransformer _ (RasterizedInfo (V4 x y _ _) _ _) = colorFunc $ Complex (x / 125.0 - 2) (y / 125.0 - 2)
 
 colorFunc :: Complex -> V3 (S F Float)
-colorFunc c = V3 r g b
+colorFunc c = V3 r' g' b'
   where
-    z = Complex 0.0 0.0
     fractl :: S F Int
-    fractl = fractal c z
-    r = 0.0
-    g = (/64) $ toFloat $ mod' 256 fractl
-    b = 0.0
+    fractl = fractal c
+    color1 = (toFloat fractl) / (toFloat maxIters)
+    r' = color1
+    g' = color1
+    b' = color1
 
-magnitude :: Complex -> S F Float
-magnitude (Complex r i) = sqrt (r * r + i * i)
+mag :: Complex -> S F Float
+mag (Complex r' i') = sqrt (r' * r' + i' * i')
 
-magnitude2 :: Complex -> S F Float
-magnitude2 (Complex r i) = r * r + i * i
+mag2 :: Complex -> S F Float
+mag2 (Complex r' i') = (r' * r') + (i' * i')
 
 
 
@@ -76,16 +78,24 @@ data Complex = Complex {
   i :: S F Float
 }
 
-complexToPair :: Complex -> (S F Float, S F Float)
-complexToPair (Complex r i) = (r, i)
+cTP :: Complex -> (S F Float, S F Float)
+cTP (Complex r' i') = (r', i')
 
-pairToComplex :: (S F Float, S F Float) -> Complex
-pairToComplex (r, i) = Complex r i
+pTC :: (S F Float, S F Float) -> Complex
+pTC (r', i') = Complex r' i'
 
-
+--frIter :: (S F Float, S F Float) -> (S F Float, S F Float) -> (S F Float, S F Float)
+frIter :: (S F Float, S F Float) -> (S F Float, S F Float)
+--frIter (zr,zi) (cr,ci) = (zr * zr - zi * zi + cr, 2 * zr * zi + ci)
+--frIter (zr,zi) = (zr * zr - zi * zi, zr * zi + zr * zi)
+--frIter (zr,zi) = (zr - zi, zi + zr) -- already bugged
+--frIter (zr',zi') = (0, zi' + zr' + 0.1)
+frIter (zr',zi') = (zi' + zr' + 0.1, 0)
+-- ( 0.2, 1.75)
+-- (-0.2, 1.75)
 
 maxIters :: S F Int
-maxIters = 2^8
+maxIters = 10
 
 myMult :: Complex -> Complex -> Complex
 myMult (Complex xr xi) (Complex yr  yi) = Complex (xr * yr - xi * yi) (xr * yi + xi * yr)
@@ -93,10 +103,11 @@ myMult (Complex xr xi) (Complex yr  yi) = Complex (xr * yr - xi * yi) (xr * yi +
 myPlus :: Complex -> Complex -> Complex
 myPlus (Complex xr xi) (Complex yr  yi) = Complex (xr + yr) (xi + yi)
 
-fractal :: Complex -> Complex -> S F Int
-fractal c (Complex zr zi) = fst $ while mcond miter ( 0, (zr,zi))
+fractal :: Complex -> S F Int
+fractal c = mod' ( fst $ while mcond miter (0, cTP c)) maxIters
   where
-      miter :: (S F Int, (S F Float, S F Float)) ->  (S F Int, (S F Float, S F Float))
-      miter (i', (zr', zi')) = (i' + 1, complexToPair $ myPlus (myMult (Complex zr' zi') (Complex zr' zi')) c)
+      miter :: (FInt, (FFloat, FFloat)) ->  (FInt, (FFloat, FFloat))
+--      miter (i', z') = (i' + 1, frIter z' (cTP c))
+      miter (i', z') = (i' + 1, frIter z')
       mcond :: (S F Int, (S F Float, S F Float)) -> S F Bool
-      mcond (i', (zr', zi')) = (i' <* maxIters) &&* ((magnitude2 (Complex zr' zi')) <* 1024.0)
+      mcond (i', z') = (i' <* maxIters) &&* ((mag2 (pTC z')) <* 4.0)
